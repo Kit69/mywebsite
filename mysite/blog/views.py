9671d80bd1b90070ea229dba245e781bdc30ea1b
Kit69 <bouavongk@gmail.com>
2018-05-21 21:47:56 +0700

    Added using class base view step.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,16 @@
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
 # List out all post with status = 'published'
 def post_list(request):
     object_list = Post.published.all()
